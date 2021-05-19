9e1ec0645cd497236be975abcb29b14cc3645c5e
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-05-19 15:17:56 -0700

    Updated SchoolView.js to use AppContext.

--- src/SchoolView.js
+++ src/SchoolView.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import SpellList from './SpellList.js';
+import AppContext from './AppContext.js';
 
 
-function SchoolView(props){
+function SchoolView(){
+    
+    const context = useContext(AppContext);
     const [description, setDescription] = useState("");
 
     useEffect( () => {
-        axios.get(props.apiroot+"/schools/"+props.school_id).then((school) => {setDescription(school.data.description)});
+        axios.get(context.apiroot+"/schools/"+context.school).then((school) => {setDescription(school.data.description)});
     });
 
     return(
@@ -18,9 +20,7 @@ function SchoolView(props){
                     <div className="summary-box">{description}</div>
                     <div className="summary-spacer-end">&nbsp;</div>
                 </div>
-                <SpellList apiroot={props.apiroot} school_id={props.school_id} updatePage={props.updatePage}
-                           updateSchool={props.updateSchool} updateSpell={props.updateSpell}
-                           updateHeaderTitle={props.updateHeaderTitle} updateType={props.updateType} />
+                <SpellList />
             </span>
     );
 }
