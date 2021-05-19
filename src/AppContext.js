3f8400de3f895c8f4f8a1f5c3df2141eaafdab79
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-05-19 14:38:05 -0700

    Creating AppContext.js
    When looking into my issues with context, I learned that, of course, I was overthinking it. This code uses createContext and useState to
    achieve what I was trying to do with hand created methods. Don't hand roll what someone else has already created and debugged.
    I am entering this through the github website, so I will test it properly when I get home. After I get this working, I am moving to using
    feature branches to avoid similar issues in future being on the master branch, even if I am working alone and on alpha code.

---
+++ src/AppContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+
+export const AppContextProvider = ({ children }) => {
+	const [page, setPage]               = useState(indexPage);
+	const [school, setSchool]           = useState("all");
+	const [spell, setSpell]             = useState(0);
+	const [type, setType]               = useState(0);
+	const [headerTitle, setHeaderTitle] = useState("FluxRPG Spell List");
+	const [apiroot, setApiroot]         = "http://localhost:8080/api";
+
+	return (
+		<AppContext.Provider
+			value={{
+				page,
+				school,
+				spell,
+				type,
+				headerTitle,
+				apiroot,
+				setPage,
+				setSchool,
+				setSpell,
+				setType,
+				setHeaderTitle,
+				setApiroot
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
